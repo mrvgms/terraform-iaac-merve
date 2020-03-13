b671187c97e6fa24c5c348a736a9133976aa77f9
Merve Gumuscibik <mgumuscibik@gmail.com>
2020-03-12 19:40:52 -0500

    changes made

--- VPC/data_source/ec2.tf
+++ VPC/data_source/ec2.tf
@@ -48,13 +48,13 @@ output "CENTOS_AMI_ID" {
 
 resource "aws_key_pair" "provisioner" {
   key_name   = "provisioner-key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}""
-
-
-resource "aws_key_pair" "provisioner" {
-  key_name   = "provisioner-key"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.centos.id}"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.provisioner.key_name}"
 
   tags = {
     Name = "HelloWorld"
