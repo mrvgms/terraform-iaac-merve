4ffbb60cc19b832d7bf179645adf7f952d3e1fdc
Merve Gumuscibik <mgumuscibik@gmail.com>
2020-03-14 15:33:49 -0500

    changes made

--- 3-tier-app/app.tf
+++ 3-tier-app/app.tf
@@ -2,5 +2,5 @@
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
-  subnet_id = "${data.terraform_remote_state.dev.Subnet1}"
+  subnet_id = "${data.terraform_remote_state.dev.subnet1}"
 }
