b6f60ea50e2ddd5b64ae4f0cce05256b15f5b26c
Merve Gumuscibik <mgumuscibik@gmail.com>
2020-03-14 16:30:33 -0500

    changes made

---
+++ 3-tier-app/elb.tf
@@ -0,0 +1,36 @@
+resource "aws_elb" "bar" {
+  name               = "wordpress-terraform-elb"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+  access_logs {
+    bucket        = "foo"
+    bucket_prefix = "bar"
+    interval      = 60
+  }
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  instances                   = ["i-01e763edd88d04c05"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "foobar-terraform-elb"
+  }
+}
