3185b484a479452838984d809a2b77841750c932
Merve Gumuscibik <mgumuscibik@gmail.com>
2020-03-14 16:09:27 -0500

    fized bugs

--- 3-tier-app/app.tf
+++ 3-tier-app/app.tf
@@ -1,6 +1,23 @@
+module "wordpress" {
+  source = "terraform-aws-modules/autoscaling/aws"
 
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-  subnet_id = "${data.terraform_remote_state.dev.subnet1}"
+  # Launch configuration
+  lc_name = "wordpress-lc"
+
+  image_id        = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.allow_tls.id}"]
+
+  # Auto scaling group
+  asg_name                  = "wordpress-asg"
+  vpc_zone_identifier       = [
+                                "${data.terraform_remote_state.dev.subnet1}",
+                                "${data.terraform_remote_state.dev.subnet2}", 
+                                "${data.terraform_remote_state.dev.subnet3}"
+                             ]
+  health_check_type         = "EC2"
+  min_size                  = 3
+  max_size                  = 5
+  desired_capacity          = 3
+  wait_for_capacity_timeout = 0
 }
