b1db24841a0225fce5510c28842177d4ba8fef85
Merve Gumuscibik <mgumuscibik@gmail.com>
2020-03-14 15:20:42 -0500

    changes made

--- 3-tier-app/output.tf
+++ 3-tier-app/output.tf
@@ -1,3 +1,30 @@
-output "full_list" {
-  value = "${data.terraform_remote_state.dev.*}"
+output "VPC_ID" {
+  value = "${data.terraform_remote_state.dev.VPC_ID}"
+}
+
+output "Subnet1" {
+  value = "${data.terraform_remote_state.dev.subnet1}"
+}
+
+output "Subnet2" {
+  value = "${data.terraform_remote_state.dev.subnet2}"
+}
+
+output "Subnet3" {
+  value = "${data.terraform_remote_state.dev.subnet3}"
+}
+
+
+output "Private_Subnet1" {
+  value = "${data.terraform_remote_state.dev.Private_Subnet1}"
+}
+
+
+output "Private_Subnet2" {
+  value = "${data.terraform_remote_state.dev.Private_Subnet2}"
+}
+
+
+output "Private_Subnet3" {
+  value = "${data.terraform_remote_state.dev.Private_Subnet3}"
 }
